c33c9be1d3a9ed9e46d9b0ed17467004f71938d6
sonidonelson <simonssheyla81@gmail.com>
2024-10-18 18:25:45 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+  let slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slides[slideIndex-1].style.display = "block";  
+}
+
+// Cambiar automáticamente cada 3 segundos
+setInterval(function(){ plusSlides(1) }, 5000);
